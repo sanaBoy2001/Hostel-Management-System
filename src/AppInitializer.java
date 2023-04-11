ad159da7bc910a43b1cacf81c4ef1a0bc856c7ce
sanaBoy2001 <www.allinone2001@gmail.com>
2023-04-11 21:43:23 +0530

    Appinitializer

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class git AppInitializer extends Application {
+public class AppInitializer extends Application {
 
     public static void main(String[] args) {
         launch(args);
